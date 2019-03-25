f922deae0d6dc1e70b08724d724e78c2b3e0ce9c
AndyHart98 <36505661+AndyHart98@users.noreply.github.com>
2019-03-25 14:12:32 +0000

    Delete hamburgerMenu.js

--- integrated-project-3-master/WEBSITE/scripts/hamburgerMenu.js
+++
@@ -1,9 +0,0 @@
-/*This code was taken from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp this code will remove or add the responsive class when the hamburger icon is clicked*/
-function hamburgerMenu() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
