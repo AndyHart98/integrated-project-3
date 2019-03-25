1692fed3ff4bfdefbba75c654a1468b5cb9fc487
RossMcGregor <47359301+RossMcGregor@users.noreply.github.com>
2019-03-25 12:54:03 +0000

    Add files via upload

--- Flood/FloodScript.js
+++ Flood/FloodScript.js
@@ -5,15 +5,16 @@ $(document).ready(function(){
 		
 		$.ajax({
 			
-			url: "http://environment.data.gov.uk/flood-monitoring/id/floods/105409",
+			url: "https://environment.data.gov.uk/flood-monitoring/id/floods",
 			type: "GET",
 			dataType: "json",
 			success: function(FloodData){
+				//for (j = 0; j < FloodData.items.length; j++) {
 				var widget = show(FloodData);
 				$("#show").html(widget);
+				//}
 			}
 		});
-		
 		
 	});
 });
@@ -21,8 +22,28 @@ $(document).ready(function(){
 
 function show(FloodData){
 		
-		return "<h2>County = " + FloodData.items.floodArea.county + "</h2>" +
-		       "<h3>Description = " + FloodData.items.message + "</h3>" +
-			   "<h3>Severity level = " + FloodData.items.severity + "</h3>" +
-			   "<h3>Is the area tidal? = " + FloodData.items.isTidal + "</h3>"  
+		
+		
+		
+		
+		
+		
+		for (i = 0; i < FloodData.items.length; i++) {
+		var obj = FloodData.items[i];
+		console.log(obj);
+		document.write("<h2>County = " + FloodData.items[i].floodArea.county + "</h2>" +
+		               "<h3>Description = " + FloodData.items[i].message + "</h3>" +
+					   "<h3>Severity level = " + FloodData.items[i].severity + "</h3>" +
+					   "<h3>Is the area tidal? = " + FloodData.items[i].isTidal + "</h3>" +
+					   "<h3>Severity Level Number = " + FloodData.items[i].severityLevel + "</h3>")
+		
+		
+		
+		
+		
+		//return "<h2>County = " + FloodData.items[i].floodArea.county + "</h2>" +
+		       //"<h3>Description = " + FloodData.items[i].message + "</h3>" +
+			  // "<h3>Severity level = " + FloodData.items[i].severity + "</h3>" +
+			   //"<h3>Is the area tidal? = " + FloodData.items[i].isTidal + "</h3>"  
+		}
 	}
